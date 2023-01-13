841ab3e1f5d03ec2b8c87a86108aaf2f088e98eb
Logan Reese <lreese2112@gmail.com>
2023-01-13 03:01:56 +0000

    pic look impossible

---
+++ app.js
@@ -0,0 +1,37 @@
+
+alert("Hello Weary Traveler!");
+
+let usersName = prompt("What be thy name?");
+
+if (usersName){
+    console.log("Et tu," + usersName + "?");
+    document.write("Welcome ",usersName,", you look parched!");
+} else {
+    console.log("Weary one.. Name please...");
+    usersName = prompt ("I SAY! BY THE BEARD OF ZEUS AND LEG HAIR OF HADES!! TELL ME THY NAME! ")
+}
+let ageOfman = prompt ("How old do thy bones smell to thee? AHEM how old am I?... me. It's Logan.")
+
+while (ageOfman !== '30'){
+    ageOfman = prompt("Nuh uh UH. Youd didn't say the magic wor...number.")
+}
+
+
+//This isn't working and im going to die trying.
+
+// let waterNumber = prompt("Thy fair noble human, prepare for thirst quench!");
+// while (waterNumber < 5){
+//         waterNumber = prompt("MORE WATERS! PRAISE THE WATER!");
+//     }
+
+//     for (let i = 0; i < waterNumber; i++){
+//         document.write(<img src="quench.jpg"/>)
+//     }
+
+
+
+
+
+
+
+
